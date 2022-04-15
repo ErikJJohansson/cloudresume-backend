4eba252b3a60428df71a4ad9995413dfb2f6674d
ErikJJohansson <erik.j.johansson@gmail.com>
2022-04-15 16:00:06 -0700

    Update image to alpine python 3.8

--- flask-container/app.py
+++ flask-container/app.py
@@ -1,5 +1,6 @@
 #app.py
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
