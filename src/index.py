e3cad38cbe54d1084b1494889062902ff8d5e0c1
ErikJJohansson <erik.j.johansson@gmail.com>
2022-05-09 18:58:22 -0700

    Directory cleanup, remove containers

---
+++ src/index.py
@@ -0,0 +1,36 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloud-resume-challenge')
+
+def get_count():
+    response = table.query(
+        KeyConditionExpression=Key('ID').eq('visitors')
+        )
+    count = response['Items'][0]['visitors']
+    return count
+
+def lambda_handler(event, context):
+    response = table.update_item(     
+        Key={        
+            'ID': 'visitors'
+        },   
+        UpdateExpression='ADD ' + 'visitors' + ' :inc',
+        ExpressionAttributeValues={        
+            ':inc': 1    
+        },    
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Credentials': '*',
+            'Content-Type': 'application/json'
+        },
+        'body': get_count()
+    }
